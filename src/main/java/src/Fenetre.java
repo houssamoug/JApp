0940babc2fd16d862e7162b8e14953c3955aa352
Houssam <Houssam@LAPTOP-HM>
2017-01-13 14:30:55 +0000

    comit1

--- src/main/java/src/Fenetre.java
+++ src/main/java/src/Fenetre.java
@@ -1,33 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package src;
 
-/**
- *
- * @author Administrateur
- */
-
 import javax.swing.JFrame;
- //test
-//test2
+import javax.swing.JLabel;
+
 public class Fenetre {
   public static void main(String[] args){
-
     JFrame fenetre = new JFrame();
+      JLabel label = new JLabel("MST-RSI2 : Ingénierie logiciel - Méthodes et outils d'industrialisation et d'intégration");
                 
     //Définit un titre pour notre fenêtre
     fenetre.setTitle("Fenêtre");
     //Définit sa taille : 400 pixels de large et 100 pixels de haut
-    fenetre.setSize(500, 200);
+    fenetre.setSize(400, 100);
     //Nous demandons maintenant à notre objet de se positionner au centre
     fenetre.setLocationRelativeTo(null);
     //Termine le processus lorsqu'on clique sur la croix rouge
     fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    fenetre.add(label);
     //Et enfin, la rendre visible        
     fenetre.setVisible(true);
   }       
-  // ce ci est un coment
 }
